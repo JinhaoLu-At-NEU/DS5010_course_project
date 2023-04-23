e1c1612c1cf621755559680f4f8c657d34d7597c
Jinhao Lu <95664828+JinhaoLu-At-NEU@users.noreply.github.com>
2023-04-23 16:14:20 -0400

    Update handy_sqlite_demo.py

--- demo/handy_sqlite_demo.py
+++ demo/handy_sqlite_demo.py
@@ -1,5 +1,4 @@
 import handy_sqlite as hg
-import csv
 import sqlite3
 
 
@@ -9,10 +8,12 @@ doctor_entity = [('Name','TEXT'),('Depart_ID','TEXT'),('Doc_ID','TEXT')]
 #create objects/tables
 doctor = hg.sqlite_data_table(table_name = 'Doctor', table_entity = doctor_entity)
 
+#convert to sql create table statement
+doctor.create_table()
+
 #create dummy data
 dummy_data = open('dummy_data.csv','w')
 dummy_list = []
-
 for i in range(20):
   dummy_list.append(['doc'+str(i),i,i%2])
 
@@ -22,35 +23,31 @@ with dummy_data:
 
 dummy_data.close()
 
-#create insert statement
+#Generate insert statement
 doctor.insert_table('dummy_data.csv','insert_doc.sql')
 
 
-#create sqlite db file
+#create sqlite database 
 conn = sqlite3.connect('demo.db')
 cursor = conn.cursor()
 
 doctor = open('Doctor.sql','r')
 insert_doc = open('insert_doc.sql','r')
 
-#execute create table statement
+#create data table wth the output
 cursor.execute(doctor.read().replace('\n',''))
 
-#execute insert table statement
 for i in insert_doc.read().split(';'):
   i = i.replace('\n','')
   cursor.execute(i)
 
-#execute select statement 
 cursor.execute('select * from Doctor limit 5;')
 
-#featch result
 result = cursor.fetchall()
-
-
-print(result)
 
 doctor .close()
 insert_doc.close()
 cursor.close()
 conn.close()
+
+print(result)
