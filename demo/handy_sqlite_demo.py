620044830f8d749da73a9ab710a08b38676ddf7c
Jinhao Lu <95664828+JinhaoLu-At-NEU@users.noreply.github.com>
2023-04-22 20:34:27 -0400

    Create handy_sqlite_demo.py

---
+++ demo/handy_sqlite_demo.py
@@ -0,0 +1,35 @@
+import handy_sqlite as hg
+import sqlite3
+
+
+#Define table entities
+doctor_entity = [('Name','TEXT'),('Depart_ID','TEXT'),('Doc_ID','TEXT')]
+
+#create objects/tables
+doctor = hg.sqlite_data_table(table_name = 'Doctor', table_entity = doctor_entity)
+
+#convert to sql create table statement
+doctor.create_table()
+
+#create dummy data
+dummy_data = open('dummy_data.csv','w')
+for i in range(20):
+  dummy_data.write('d'+str(i)+','+str(i)+','+str(i%2)+'\n')
+dummy_data.close()
+
+#Generate insert statement
+doctor.insert_table('dummy_data.csv','insert_doc.sql')
+
+
+#create sqlite database 
+conn = sqlite3.connect('demo.db')
+cursor = conn.cursor()
+
+doctor = open('Doctor.sql','r')
+
+#create data table wth the output
+cursor.execute(doctor.read().replace('\n',''))
+
+
+cursor.close()
+conn.close()
